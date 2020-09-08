4ee0a7a9faacb0f8683fa80b0132b2b0f35fd47b
Proyectocompu <kuani2443t@gmail.com>
2020-09-08 16:09:22 -0600

    Menu responsive completado

--- js/jsmenu.js
+++ js/jsmenu.js
@@ -1,15 +1,36 @@
-var botonlis4 = document.getElementById("lisboton4");
-var boton4 = document.getElementById("boton4");
+var botonlis4 = document.getElementById("lisBotondes");
+var boton4 = document.getElementById("botondes");
 
-
-function desplegarboton4() {
+function desplegarBoton4() {
   botonlis4.classList.add("mostrar");
-
 }
 
-function ocultarBoton4(){
-    botonlis4.classList.remove("mostrar");
+function ocultarBoton4() {
+  botonlis4.classList.remove("mostrar");
 }
 
-boton4.addEventListener("mouseover", desplegarboton4,true);
-boton4.addEventListener("mouseout", ocultarBoton4,true);
+boton4.addEventListener("mouseover", desplegarBoton4, true);
+boton4.addEventListener("mouseout", ocultarBoton4, true);
+
+/*PROGRAMACIÓN DEL MENU EN MODO MOBIL*/
+
+var iconoMenuMobil = document.getElementById("iconoMenu"),
+  botonCerrar = document.getElementById("iconoCerrar");
+
+var nav = document.getElementById("nav"),
+  botonesNav = document.getElementById("ulNav");
+
+function desplegarMenuMobil() {
+  nav.classList.add("activarNav");
+  botonesNav.classList.add("activarBotones");
+  botonCerrar.classList.remove("none");
+}
+
+function cerrar() {
+  nav.classList.remove("activarNav");
+  botonesNav.classList.remove("activarBotones");
+  botonCerrar.classList.add("none");
+}
+
+iconoMenuMobil.addEventListener("click", desplegarMenuMobil, true);
+botonCerrar.addEventListener("click", cerrar, true);
